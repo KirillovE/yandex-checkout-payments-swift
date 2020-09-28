613ffd2bb0240be6a27705cdf3800b3b3d68716e
KirillovE <evgeniy.kirillov@live.ru>
2020-09-28 09:45:50 +0300

    Fixing errors

--- YandexCheckoutPayments/Private/Atomic Design/Templates/ContractTemplate.swift
+++ YandexCheckoutPayments/Private/Atomic Design/Templates/ContractTemplate.swift
@@ -72,8 +72,8 @@ final class ContractTemplate: UIViewController {
 
     private var backgroundStyle = UIView.Styles.grayBackground
 
-    private lazy var blurEffectStyle: UIBlurEffect.YandexStyle = {
-        let style: UIBlurEffect.YandexStyle
+    private lazy var blurEffectStyle: UIBlurEffect.Style = {
+        let style: UIBlurEffect.Style
         if #available(iOS 13.0, *) {
             style = .systemUltraThinMaterial
         } else {
